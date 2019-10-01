ab7c7c98d336078454973bf802d2402973ea0efb
James Batsell <jamesbatsell05@gmail.com>
2019-10-01 23:42:56 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,7 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
-
+  
   def self.all
     @@all
   end
@@ -9,51 +9,60 @@ class Song
   def save
     self.class.all << self
   end
-  
+
   def self.create
     song = self.new
     song.save
     song
   end
   
-  def self.new_by_name(song_name)
-    song = Song.create
-    song.name = song_name
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.create
+    song.name = name
     song
   end
   
-  def self.create_by_name(song_name)
-    self.new_by_name(song_name)
-    @@all << song
+  def self.find_by_name(name)
+    song = self.all.select { |song| song.name == name}
+    song[0]
+  end
+
+  def self.find_or_create_by_name(name)
+    if !find_by_name(name)
+      create_by_name(name)
+    else
+      find_by_name(name)
+    end
   end
   
-  def self.find_by_name(song_name)
-    self.all.find {|song| song.name == song.name}
+  def self.alphabetical
+    self.all.sort_by { |song| song.name[0]}
   end
   
-  def self.find_or_create_by_name(song_name)
-    self.find_by_name(song_name) || self.create_by_name(song_name)
-end
-
-
-def self.alphabetical
-  self.all.sort_by {|song| song.name}
-end
-
-def self.new_from_filename(song)
-  split_file = song.split(" - ")
-  artist = split_file[0]
-  song_name = split_file[1].split(".")[0]
- song =  self.new_by_name(song_name)
- song.artist_name = artist
- #song.name = song_name
-end
-
-def self.create_from_filename(song)
-  self.new_from_filename(song)
-end
-
-def self.destroy_all
-  self.all.clear
-end
+  def self.new_from_filename(filename)
+    song_data = filename.sub!(/.mp3/,'').split(' - ')
+    song = self.create
+    song.artist_name = song_data[0]
+    song.name = song_data[1]
+    song
+  end
+  
+  def self.create_from_filename(filename)
+    song_data = filename.sub!(/.mp3/,'').split(' - ')
+    song = self.create
+    song.artist_name = song_data[0]
+    song.name = song_data[1]
+    song
+  end
+  
+  def self.destroy_all
+    self.all.clear
+  end
+  
 end
